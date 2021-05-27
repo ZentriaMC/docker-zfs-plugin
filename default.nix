4f6aeb1babf1589019b27da31b6d2d2df50be3f3
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-27 20:35:41 +0300

    Add derivation meta

--- default.nix
+++ default.nix
@@ -12,4 +12,11 @@ buildGoModule rec {
 
   vendorSha256 = "0sxcgw1hr7skl3x5jzzk8k3pcd99f2q3cycvqcakvbbrr3mxz35d";
   subPackages = [ "." ];
+
+  meta = with stdenv.lib; {
+    description = "Docker volume plugin for creating persistent volumes as dedicated zfs datasets.";
+    homepage = "https://github.com/ZentriaMC/docker-zfs-plugin";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
 }
